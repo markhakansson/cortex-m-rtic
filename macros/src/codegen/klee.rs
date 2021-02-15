8628e22b8af66ae94073a8ee6c6b075047133770
Mark Hakansson <mark.hakansson@live.se>
2021-02-15 14:36:16 +0100

    Use default values on resources with klee feature

--- macros/src/codegen/klee.rs
+++ macros/src/codegen/klee.rs
@@ -14,31 +14,31 @@ pub fn codegen(app: &App, analysis: &Analysis) -> Vec<TokenStream2> {
     let mut resource_list = vec![];
     let mut task_number: u32= 0;
     
+    // When matching make the task ID symbolic
     res.push(quote!(
         let mut task = 0;
         klee_make_symbolic!(&mut task, "task");
     ));
 
+    // Make resources as symbolic
     for (name, resource, expr, _) in app.resources(analysis){
         let ty = &resource.ty;
         let mangled_name = util::mangle_ident(&name);
-        {
-            resource_list.push(quote!(
-                let mut #name = 0;
-                klee_make_symbolic!(&mut #name, "#name");
-            ));
+        let name_str = name.to_string();
 
-            if let Some(expr) = expr {
-                resource_list.push(quote!(
-                    #mangled_name = #name;    
-                ));
-            } else {
-                resource_list.push(quote!(
-                    #mangled_name.as_mut_ptr().write(#name);
-                ));
-            };
-        }
+        let assignment = match expr {
+            Some(_) => quote!(#mangled_name = #name;),
+            None => quote!(#mangled_name.as_mut_ptr().write(#name);)
+        };
+
+        resource_list.push(quote!(
+            let mut #name = Default::default();
+            klee_make_symbolic!(&mut #name, #name_str);
+            #assignment
+        ));
     }
+    
+    // Add tasks for KLEE to match
     for (name, _task) in &app.hardware_tasks {
         task_list.push(quote!(
             #task_number => #app_path::#name(#name::Context::new(&rtic::export::Priority::new(1))),
