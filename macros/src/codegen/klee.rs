d22b3131868e7fbf177668570fbe9a1071401312
Mark Hakansson <mark.hakansson@live.se>
2021-02-18 09:17:58 +0100

    Clear up symbols in klee test harness

--- macros/src/codegen/klee.rs
+++ macros/src/codegen/klee.rs
@@ -14,36 +14,17 @@ pub fn codegen(app: &App, analysis: &Analysis) -> Vec<TokenStream2> {
     let mut match_stmts = vec![];
     let mut symbolic_resources= vec![];
     let mut task_number: u32= 0;
-    
-    // When matching make the task ID symbolic
-    test_harness.push(quote!(
-        let mut task = 0;
-        klee_make_symbolic!(&mut task, "task");
-    ));
 
     // Make resources symbolic
     for (name, _, _, _) in app.resources(analysis){
         let mangled_name = util::mangle_ident(&name);
-        let name_as_str = name.to_string();
+        
+        let name_as_str: String = app_name.to_string() + "::resources::"+ &name.to_string();
 
         // Does only work for core types
         symbolic_resources.push(quote!(
             klee_make_symbolic!(&mut #mangled_name, #name_as_str);
         ));
-    }
-    
-    // Fetch all tasks for KLEE to match
-    for (name, _task) in &app.hardware_tasks {
-        task_list.push(quote!(
-            #task_number => #app_path::#name(#name::Context::new(&rtic::export::Priority::new(1))),
-        ));
-        task_number += 1;
-    }
-    for (name, _task) in &app.software_tasks{
-        task_list.push(quote!(
-            #task_number => #app_path::#name(#name::Context::new(&rtic::export::Priority::new(1))),
-        ));
-        task_number += 1;
     }
     
     // Add init function
@@ -55,16 +36,36 @@ pub fn codegen(app: &App, analysis: &Analysis) -> Vec<TokenStream2> {
             #app_path::#init_name(#init_name::Context::new(core.into()));
         },
     ));
+    task_number += 1;
 
+    // Fetch all tasks for KLEE to match
+    for (name, _task) in &app.hardware_tasks {
+        task_list.push(quote!(
+            #task_number => #app_path::#name(#name::Context::new(&rtic::export::Priority::new(1))),
+        ));
+        task_number += 1;
+    }
+for (name, _task) in &app.software_tasks{
+        task_list.push(quote!(
+            #task_number => #app_path::#name(#name::Context::new(&rtic::export::Priority::new(1))),
+        ));
+        task_number += 1;
+    }
+    
     // Insert all tasks inside a match
     match_stmts.push(quote!(
-        match task {
+        match task_id {
             #(#task_list)*
             _ => ()
         }
     ));
     
-    test_harness.append(&mut symbolic_resources);
-    test_harness.append(&mut match_stmts);
+    // Finish test harness
+    test_harness.push(quote!(
+        let mut task_id = 0;
+        klee_make_symbolic!(&mut task_id, "__klee_task_id");
+        #(#symbolic_resources)*
+        #(#match_stmts)*
+    ));
     test_harness
 } 
