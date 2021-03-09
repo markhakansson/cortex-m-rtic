800af27f1abedad54219069214cfe7b97fad73eb
Mark Hakansson <mark.hakansson@live.se>
2021-03-09 13:47:34 +0100

    Update klee replay

--- macros/src/codegen/hardware_tasks.rs
+++ macros/src/codegen/hardware_tasks.rs
@@ -8,6 +8,7 @@ use crate::{
     codegen::{locals, module, resources_struct},
 };
 
+#[cfg(not(feature = "klee-replay"))]
 /// Generate support code for hardware tasks (`#[exception]`s and `#[interrupt]`s)
 pub fn codegen(
     app: &App,
@@ -56,6 +57,108 @@ pub fn codegen(
                         #name::Context::new(&rtic::export::Priority::new(PRIORITY))
                     )
                 });
+            }
+        ));
+
+        let mut needs_lt = false;
+
+        // `${task}Resources`
+        if !task.args.resources.is_empty() {
+            let (item, constructor) =
+                resources_struct::codegen(Context::HardwareTask(name), &mut needs_lt, app);
+
+            root.push(item);
+
+            mod_app.push(constructor);
+        }
+
+        root.push(module::codegen(
+            Context::HardwareTask(name),
+            needs_lt,
+            app,
+            analysis,
+            extra,
+        ));
+
+        // `${task}Locals`
+        let mut locals_pat = None;
+        if !task.locals.is_empty() {
+            let (struct_, pat) = locals::codegen(Context::HardwareTask(name), &task.locals, app);
+
+            root.push(struct_);
+            locals_pat = Some(pat);
+        }
+
+        if !&task.is_extern {
+            let attrs = &task.attrs;
+            let context = &task.context;
+            let stmts = &task.stmts;
+            let locals_pat = locals_pat.iter();
+            user_tasks.push(quote!(
+                #(#attrs)*
+                #[allow(non_snake_case)]
+                fn #name(#(#locals_pat,)* #context: #name::Context) {
+                    use rtic::Mutex as _;
+                    use rtic::mutex_prelude::*;
+
+                    #(#stmts)*
+                }
+            ));
+        }
+    }
+
+    (mod_app, root, user_tasks)
+}
+
+#[cfg(feature = "klee-replay")]
+pub fn codegen(
+    app: &App,
+    analysis: &Analysis,
+    extra: &Extra,
+) -> (
+    // mod_app_hardware_tasks -- interrupt handlers and `${task}Resources` constructors
+    Vec<TokenStream2>,
+    // root_hardware_tasks -- items that must be placed in the root of the crate:
+    // - `${task}Locals` structs
+    // - `${task}Resources` structs
+    // - `${task}` modules
+    Vec<TokenStream2>,
+    // user_hardware_tasks -- the `#[task]` functions written by the user
+    Vec<TokenStream2>,
+) {
+    let mut mod_app = vec![];
+    let mut root = vec![];
+    let mut user_tasks = vec![];
+
+    for (name, task) in &app.hardware_tasks {
+        let locals_new = if task.locals.is_empty() {
+            quote!()
+        } else {
+            quote!(#name::Locals::new(),)
+        };
+
+        let symbol = task.args.binds.clone();
+        let priority = task.args.priority;
+
+        let app_name = &app.name;
+        let app_path = quote! {crate::#app_name};
+        mod_app.push(quote!(
+            #[allow(non_snake_case)]
+            #[no_mangle]
+            unsafe fn #symbol() {
+                // START TASK HW BKPT HERE
+                asm!("bkpt 2");
+
+                const PRIORITY: u8 = #priority;
+
+                rtic::export::run(PRIORITY, || {
+                    #app_path::#name(
+                        #locals_new
+                        #name::Context::new(&rtic::export::Priority::new(PRIORITY))
+                    )
+                });
+                // END TASK HW BKPT HERE
+                asm!("bkpt 253");
             }
         ));
 
